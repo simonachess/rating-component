89b9c51cbf21fed7548d3333666b8f56cbaf7369
simonachess <simonachess@gmail.com>
2022-07-09 15:06:24 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
 
 	return (
 		<div className="h-screen flex justify-center items-start md:items-center px-4 py-7 md:px-0 md:py-0">
-			<div className={classnames(showThankYou.showThankYouPage === true && "hidden","flex flex-col w-full md:w-[412px] md:h-[416px] bg-gradient-to-t from-[#232A34] to-[#181E27] rounded-3xl p-8")}>
+			<main className={classnames(showThankYou.showThankYouPage === true && "hidden","flex flex-col w-full md:w-[412px] md:h-[416px] bg-gradient-to-t from-[#232A34] to-[#181E27] rounded-3xl p-8")}>
 				<span className="h-[51px] w-[51px] rounded-full flex justify-center items-center bg-[#262E38]">
 					<img src="/img/icon-star.svg" alt="star"></img>
 				</span>
@@ -29,7 +29,7 @@ export default function App() {
 					}
 				</div>
 				<button onClick={()=>{setShowThankYou({ showThankYouPage: true, selectedRating: rating })}} className="w-full font-bold text-base uppercase rounded-full p-3 bg-[#FC7614] text-[#FFFFFF] tracking-[3px] hover:bg-[#FFFFFF] hover:text-[#FC7614] transition ease-in-out delay-100">Submit</button>
-			</div>
+			</main>
 			<div className={classnames(showThankYou.showThankYouPage === false && "hidden","flex flex-col items-center justify-center w-full md:w-[412px] md:h-[416px] bg-gradient-to-t from-[#232A34] to-[#181E27] rounded-3xl py-6 px-8")}>
 				<img src="/img/illustration-thank-you.svg" width="162" height="108" alt="thank-you"></img>
 				<span className="text-[#FC7614] rounded-full bg-[#262E38] mt-6 py-2 px-6">You selected {rating} out of 5</span>
